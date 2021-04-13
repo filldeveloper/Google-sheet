907765e4ab65c0e185f48860efc559f1b730d55c
filldeveloper <felipebarreto.silva@hotmail.com>
2021-04-13 19:01:20 -0300

    Bot Telegram

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,62 +1,64 @@
 import os
+import logging
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from credenciais import token_bot_zabbix
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import Filters
 from telegram.ext import MessageHandler
+from telegram.ext import ConversationHandler
 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 def start(update, context):
-    message = 'Seja bem vindo ao Bot Telegram'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    message = update.message.reply_text(
+        'Seja bem vindo ao Bot Telegram do Felipe!\n\nEscolha uma opção: \n1 - Felipe\n2 - Bruno\n3 - Vavá\n4 - Silvia',
+        reply_markup=ReplyKeyboardRemove()
+    )
+    echo(update, context)
 
 
 def echo(update, context):
-    message = 'Escolha uma opção: \n1 - Felipe\n2 - Bruno\n3 - Vavá\n4 - Silvia'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    option = update.message.text
+    if option == '1':
+        message = 'Felipe é o filho mais velho!'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    elif option == '2':
+        message = 'Bruno é o filho mais novo!'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    elif option == '3':
+        message = 'Vavá é o pai da família!'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    else:
+        message = 'Silvia é a mãe da família!'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
 def unknown(update, context):
-    message = 'Unknown Command'
+    message = 'Mas que bosta'
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-def felipe(update, context):
-    message = 'Filho mais Velho'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
-
-def bruno(update, context):
-    message = 'Filho mais Novo'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
-def vava(update, context):
-    message = 'Pai de família'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
-def silvia(update, context):
-    message = 'Mãe e dona de casa'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-
-token = "1784430746:AAFp0MUPY-i6nk085y16AwmowXEE4JQ16X8"
+token = token_bot_zabbix
 # os.getenv('TOKEN_TG')
 
 
 def main():
     print('Start bot configuration')
     updater = Updater(token=token, use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(
-        Filters.text & (~Filters.command), echo))
-    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-    dispatcher.add_handler(MessageHandler('1', felipe))
-    dispatcher.add_handler(MessageHandler('2', bruno))
-    dispatcher.add_handler(MessageHandler('3', vava))
-    dispatcher.add_handler(MessageHandler('4', silvia))
-    # Start the bot and
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    teste = dp.add_handler(MessageHandler(
+        Filters.text & (~Filters.command), start))
+    dp.add_handler(MessageHandler(Filters.text, unknown))
+    
+    # Start the bot
     updater.start_polling()
     updater.idle()
 
