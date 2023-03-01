42d5229201a85c998abfff8dde71e1afc9973aeb
Felipe Barreto da Silva <felipebarreto.silva@hotmail.com>
2023-02-28 21:23:28 -0300

    Raspagem de dados da Caixa funcionando

--- funcoes_planilha_credito.py
+++ funcoes_planilha_credito.py
@@ -10,13 +10,14 @@ import pandas as pd
 from time import sleep
 from pprint import pprint
 import credenciais
-# from telegram.ext import Updater
-# import telegram
 
 
 def extrato_bb():
 
     options = webdriver.ChromeOptions()
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
     options.add_argument('--log-level=3')
     url = 'https://www2.bancobrasil.com.br/aapf/login.html?1638208647003#/acesso-aapf-agencia-conta-1'
 
@@ -26,8 +27,7 @@ def extrato_bb():
         )
 
     chrome.get(url)
-
-    sleep(10)
+    sleep(7)
 
     agencia = credenciais.agencia_bb
     conta = credenciais.conta_bb
@@ -39,22 +39,22 @@ def extrato_bb():
     chrome.find_element(
         By.ID,"numeroContratoOrigem"
         ).send_keys(conta, Keys.ENTER)
-
     sleep(4)
+
     # Preencher o campo senha e apertar Enter
     chrome.find_element(By.ID,"senhaConta").send_keys(senha, Keys.ENTER)
+    sleep(8)
 
-    sleep(12)
-    # Digita C,artões no campo de busca e aperta Enter
+    # Digita Cartões no campo de busca e aperta Enter
     chrome.find_element(By.ID,"acheFacil").send_keys("cartões", Keys.ENTER)
-    sleep(4)
+    sleep(3)
+
     # Escolhe a opção de extrato e entra
     act = ActionChains(chrome)
     act.click(chrome.find_element(
         By.XPATH,"//a[contains(text(),'Cartões - Fatura - Extrato')]"
         )).perform()
-
-    sleep(10)
+    sleep(7)
 
     # Seleciona o Cartão Elo Nanquim
     chrome.find_element(By.XPATH,'//*[@id="carousel1"]/div/div/img[2]').click()
@@ -63,6 +63,7 @@ def extrato_bb():
     # Extrair a data de fechamento da próxima fatura
     chrome.find_element(By.XPATH,'//*[@id="faturasAtual"]/li[11]/a').click()
     sleep(2)
+
     elemento = chrome.find_element(
         By.XPATH,'//*[@id="fatura2"]/table/tbody/tr[4]/td/div/ul/li/span'
         )
@@ -71,6 +72,7 @@ def extrato_bb():
     fechamento_fatura_elo = soup.get_text('\n')
     print(fechamento_fatura_elo)
     sleep(5)
+
     # Clica em fatura atual para ver os histórico de compras recente
     chrome.find_element(By.XPATH,"//*[@id='faturasAtual']/li[12]").click()
     sleep(2)
@@ -110,31 +112,16 @@ def extrato_bb():
     
     # Seleciona o Cartão Smiles Visa Infinite
     chrome.execute_script("window.scrollTo(0,0)")
-
     sleep(2)
 
     # Selecionar cartão Elo Nanquim
     chrome.find_element(By.XPATH,'//*[@id="carousel1"]/div/div/img[3]').click()
-
-#     sleep(2)
-
-#     # Pegando a data de fechamento da fatura Elo Nanquimq
-#     chrome.find_element_by_xpath('//*[@id="faturasAtual"]/li[11]/a').click()
-#     sleep(2)
-
-#     elemento = chrome.find_element_by_xpath(
-#         '//*[@id="fatura2"]/table/tbody/tr[4]/td/div/ul/li/span')
-#     html_content = elemento.get_attribute('outerHTML')
-#     soup = BeautifulSoup(html_content, 'html.parser')
-#     fechamento_fatura_elo = soup.get_text('\n')
-#     sleep(2)
+    sleep(2)
 
     chrome.find_element(By.CLASS_NAME,"inactive").click()
-
     sleep(2)
 
     chrome.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-
     sleep(4)
 
     visa = []
@@ -157,6 +144,10 @@ def extrato_bb():
         lista = []
         visa = []
         for linha in historico_compras['extrato']:
+
+            if linha['Valor'] == '16.687,00':
+                continue
+            
             lista.append(linha['Data'])
             lista.append(linha['Descrição'])
             lista.append(int(linha['Valor'])/100)
@@ -165,181 +156,136 @@ def extrato_bb():
     except Exception as err:
         print(err)
 
-#     # if visa:
-#     #     lista
-#     # if smiles:
-#     #     pprint(f'Extrato Smiles: {smiles}')
-#     # if nanquim:
-#     #     pprint(f'Extrato Nanquim: {nanquim}')
-
     lista_definitiva = visa + elo_nanquim
+
+    return visa, elo_nanquim, fechamento_fatura_elo, chrome
+
+
+def extrato_caixa(chrome):
+    url = 'https://internetbanking.caixa.gov.br/sinbc/#!nb/login'
+    chrome.get(url)
+    sleep(7)
+
+    usuario = credenciais.usuario_caixa
+    chrome.find_element(By.ID,"nomeUsuario").send_keys(usuario)
+    chrome.find_element(By.NAME,"btnLogin").click()
+    sleep(4)
+
+    # Clicar no botão 
+    chrome.find_element(By.ID,'lnkInitials').click()
+    sleep(4)
+
+    # Inserção de senha no teclado virtual
+    chrome.find_element(By.XPATH,f'//*[@id="teclado"]/ul/li[{credenciais.senha_caixa[0]}]').click()
+    sleep(1)
+    chrome.find_element(By.XPATH,f'//*[@id="teclado"]/ul/li[{credenciais.senha_caixa[1]}]').click()
+    sleep(1)
+    chrome.find_element(By.XPATH,f'//*[@id="teclado"]/ul/li[{credenciais.senha_caixa[2]}]').click()
+    sleep(1)
+    chrome.find_element(By.XPATH,f'//*[@id="teclado"]/ul/li[{credenciais.senha_caixa[3]}]').click()
+    sleep(1)
+    chrome.find_element(By.XPATH,f'//*[@id="teclado"]/ul/li[{credenciais.senha_caixa[4]}]').click()
+    sleep(1)
+    chrome.find_element(By.XPATH,f'//*[@id="teclado"]/ul/li[{credenciais.senha_caixa[5]}]').click()
+    sleep(1)
+    chrome.find_element(By.XPATH,f'//*[@id="teclado"]/ul/li[{credenciais.senha_caixa[6]}]').click()
+    sleep(1)
+    chrome.find_element(By.XPATH,f'//*[@id="teclado"]/ul/li[{credenciais.senha_caixa[7]}]').click()
+    sleep(1)
+    chrome.find_element(By.ID,'btnConfirmar').click()
+    sleep(8)
+
+    # Caso exista o popup de promoção, clicar em fechar
+    try:
+        chrome.find_element(By.NAME,'btnPromoFechar').click()
+    except Exception:
+        print("Não possuía o popup de promoção")
+    sleep(3)
+
+    # Clicando em cartões na home
+    chrome.find_element(
+        By.XPATH,
+        '//*[@id="carrosselLista"]/li[2]/div[1]/div').click()
+    sleep(3)
+
+    # Clicando em Faturas para pegar a data de fechamento
+    chrome.find_element(
+        By.XPATH,
+        '//*[@id="submenu"]/div[3]/ul/li[2]/a').click()
+    sleep(3)
+
+    chrome.find_element(By.XPATH,'//*[@id="linhaTabelaInicio"]/tr[2]').click()
+    sleep(2)
+
+    elemento = chrome.find_element(By.XPATH,'//*[@id="tb_list_Inicio"]')
+    html_content = elemento.get_attribute('outerHTML')
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.find(name='table')
+    table_content = pd.read_html(str(table))[0]
+    data_fechamento = table_content[[0, 1]]
+    data = data_fechamento.to_dict('records')
+    data_definitiva = data[1][1]
+    print(f'A data de fechamento da próxima fatura é: {data_definitiva}')
+
+    # Volta para a home
+    chrome.find_element(
+        By.XPATH,
+        '//*[@id="home"]/div[1]/div[2]/div[2]/div[2]/div/div[1]/div').click()
+    sleep(2)
+
+    # Clicando em cartões
+    chrome.find_element(
+        By.XPATH,
+        '//*[@id="carrosselLista"]/li[2]/div[1]/div').click()
+    sleep(1)
+
+    # Histórico de compras
+    chrome.find_element(
+        By.XPATH,
+        '//*[@id="submenu"]/div[3]/ul/li[3]/a').click()
+    sleep(3)
+
+    # Rolar página para baixo e carregar o HTML caso esteja oculto ainda
+    chrome.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    sleep(1)
+
+    elemento = chrome.find_element(By.XPATH,'//*[@id="tb_list_Inicio"]')
+    html_content = elemento.get_attribute('outerHTML')
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.find(name='table')
+
+    extrato_full = pd.read_html(str(table))[0]
+
+    extrato = extrato_full[[0, 1]]
+
+    historico_compras = {}
+    historico_compras['extrato'] = extrato.to_dict('records')
+
+    lista = []
+    caixa = []
+    for linha in historico_compras['extrato']:
+        if 'Cancelada' in linha[0]:
+            continue
+        elif 'Negada' in linha[0]:
+            continue
+        data = linha[0].split(' - ')
+        data = data[0].split('-')
+        data = data[0] + '/' + data[1] + '/' + data[2]
+        lista.append(data)
+
+        description = linha[0].split('Aprovada')
+        description = description[0]
+        description = description[22:-1]
+        lista.append(description)
+
+        valor = linha[1].split('R$ ')
+        valor = valor[1].split(' ')
+        valor = 'R$ ' + valor[0]
+        lista.append(valor)
+        caixa.append(lista)
+        lista = []
+    
     chrome.close()
 
-    return visa, elo_nanquim, fechamento_fatura_elo
 
-
-# def extrato_caixa():
-#     option = Options()
-#     # option.headless = True
-#     chrome = webdriver.Chrome(options=option)
-#     chrome.get('https://internetbanking.caixa.gov.br/sinbc/#!nb/login')
-#     sleep(5)
-
-#     usuario = credenciais.usuario_caixa
-
-#     chrome.find_element_by_id("nomeUsuario").send_keys(usuario)
-
-#     chrome.find_element_by_name("btnLogin").click()
-
-#     sleep(5)
-
-#     chrome.find_element_by_id('lnkInitials').click()
-
-#     sleep(5)
-
-#     # Inserção de senha no teclado virtual
-#     chrome.find_element_by_xpath('//*[@id="teclado"]/ul/li[15]').click()
-#     sleep(1)
-#     chrome.find_element_by_xpath('//*[@id="teclado"]/ul/li[27]').click()
-#     sleep(1)
-#     chrome.find_element_by_xpath('//*[@id="teclado"]/ul/li[22]').click()
-#     sleep(1)
-#     chrome.find_element_by_xpath('//*[@id="teclado"]/ul/li[18]').click()
-#     sleep(1)
-#     chrome.find_element_by_xpath('//*[@id="teclado"]/ul/li[23]').click()
-#     sleep(1)
-#     chrome.find_element_by_xpath('//*[@id="teclado"]/ul/li[13]').click()
-#     sleep(1)
-#     chrome.find_element_by_xpath('//*[@id="teclado"]/ul/li[2]').click()
-#     sleep(1)
-#     chrome.find_element_by_xpath('//*[@id="teclado"]/ul/li[3]').click()
-#     sleep(1)
-#     chrome.find_element_by_id('btnConfirmar').click()
-#     sleep(5)
-
-#     try:
-#         chrome.find_element_by_name('btnPromoFechar').click()
-#     except Exception:
-#         print("Não possuia o pop'up de promoção")
-#     sleep(5)
-
-#     chrome.find_element_by_id('btnAccountsExchange').click()
-#     sleep(2)
-
-#     chrome.find_element_by_id('1').click()
-#     sleep(8)
-#     # Clicando em cartões na home
-#     chrome.find_element_by_xpath(
-#         '//*[@id="carrosselLista"]/li[2]/div[1]/div').click()
-
-#     sleep(5)
-#     # Clicandoo em Faturas para pegar a data de fechamento
-#     chrome.find_element_by_xpath(
-#         '//*[@id="submenu"]/div[3]/ul/li[2]/a').click()
-
-#     sleep(5)
-
-#     chrome.find_element_by_xpath('//*[@id="linhaTabelaInicio"]/tr[2]').click()
-
-#     sleep(5)
-
-#     elemento = chrome.find_element_by_xpath('//*[@id="tb_list_Inicio"]')
-#     html_content = elemento.get_attribute('outerHTML')
-#     soup = BeautifulSoup(html_content, 'html.parser')
-#     table = soup.find(name='table')
-#     table_content = pd.read_html(str(table))[0]
-#     data_fechamento = table_content[[0, 1]]
-#     data = data_fechamento.to_dict('records')
-#     data_definitiva = data[1][1]
-#     print(f'A data de fechamento da próxima fatura é: {data_definitiva}')
-
-#     sleep(2)
-#     # Volta para a home
-#     chrome.find_element_by_xpath(
-#         '//*[@id="home"]/div[1]/div[2]/div[2]/div[2]/div/div[1]/div').click()
-
-#     sleep(4)
-
-#     # Clicando em cartões
-#     chrome.find_element_by_xpath(
-#         '//*[@id="carrosselLista"]/li[2]/div[1]/div').click()
-
-#     sleep(5)
-
-#     # Histórico de compras
-#     chrome.find_element_by_xpath(
-#         '//*[@id="submenu"]/div[3]/ul/li[3]/a').click()
-
-#     sleep(5)
-
-#     chrome.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-
-#     sleep(5)
-
-#     elemento = chrome.find_element_by_xpath('//*[@id="tb_list_Inicio"]')
-#     html_content = elemento.get_attribute('outerHTML')
-#     soup = BeautifulSoup(html_content, 'html.parser')
-#     table = soup.find(name='table')
-
-#     extrato_full = pd.read_html(str(table))[0]
-
-#     extrato = extrato_full[[0, 1]]
-
-#     historico_compras = {}
-#     historico_compras['extrato'] = extrato.to_dict('records')
-
-#     lista = []
-#     caixa = []
-#     for linha in historico_compras['extrato']:
-#         if 'Cancelada' in linha[0]:
-#             continue
-#         elif 'Negada' in linha[0]:
-#             continue
-#         data = linha[0].split(' - ')
-#         data = data[0].split('-')
-#         data = data[0] + '/' + data[1] + '/' + data[2]
-#         lista.append(data)
-
-#         description = linha[0].split('Aprovada')
-#         description = description[0]
-#         description = description[22:-1]
-#         lista.append(description)
-
-#         valor = linha[1].split('R$ ')
-#         valor = valor[1].split(' ')
-#         valor = 'R$ ' + valor[0]
-#         lista.append(valor)
-#         caixa.append(lista)
-#         lista = []
-
-#     chrome.close()
-
-#     return caixa, data_definitiva
-
-
-# def mensagem_bot_telegram(mensagem):
-#     import requests
-#     import urllib.parse
-
-#     # Ler msgs que estão sendo mandadas para o bot
-#     chat_id = credenciais.chat_id_telegram
-#     token = credenciais.token_telegram
-#     mensagem_bot = mensagem
-#     msg_url = urllib.parse.quote(mensagem_bot)
-#     # url_base = f'https://api.telegram.org/bot{token}/getUpdates'
-#     url_send = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={msg_url}'
-#     resultado = requests.post(url_send)
-
-#     return resultado
-
-# def telegram_bot(mensagem):
-
-#     token = credenciais.token_telegram
-#     chatid = credenciais.chat_id
-#     updater = Updater(token=token, use_context=True)
-
-#     updater.bot.send_message(chat_id=chatid, text=mensagem,
-#                             parse_mode=telegram.ParseMode.MARKDOWN_V2)
-
-#     return 'Mensagem Enviada!'
-
+    return caixa, data_definitiva
--- read.py
+++ read.py
@@ -1,13 +1,10 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-from funcoes_planilha_credito import extrato_bb # extrato_caixa, telegram_bot
+from funcoes_planilha_credito import extrato_bb, extrato_caixa
 from datetime import datetime, date
 import time
 from pprint import pprint
 
-# mensagem_inicial = "Bot de controle de gastos iniciado\!"
-# telegram_bot(mensagem_inicial)
-# Chamando a função que pega os valores do extrato BB
 lista_definitiva = extrato_bb()
 
 historico_compras_visa = lista_definitiva[0]
@@ -27,18 +24,8 @@ if aba_mes == 13:
 range_aba = f'{dicionario_meses[aba_mes]}!F11'
 
 # Pegar data atual para decidir qual mês irei adicionar os dados
-data_atual = date.today()
+data_atual = date.today() 
 data_atual = f'{data_atual.day}/{data_atual.month}/{data_atual.year}'
-
-# # historico_compras_visa = [['10/12', 'LOUNGE KEY INC', 5.78],
-# #                         ['10/12', 'GOL TRANSP A*CDHZPR014216', 45.0],
-# #                         ['15/12', 'SUPERMERCADINHOS', 23.01],
-# #                         ['23/12', 'SUPERMERCADINHOS', 25.06]]
-
-# # historico_compras_nanquim = [['20/12', '99 TECNOLOGIA LTDA', 100.0],
-# #                             ['18/12', 'BE HONEST', 34.34],
-# #                             ['29/12', 'MARMITARIA VENTURA MAM', 16.0],
-# #                             ['29/12', '99 TECNOLOGIA LTDA', 100.0]]
 
 # Juntar as duas listas de gastos
 historico_compras = historico_compras_visa + historico_compras_nanquim
@@ -47,12 +34,8 @@ print(f'Fechamento da próxima fatura Visa: {fechamento_fatura}')
 print(f'Histórico de compras Visa: {historico_compras_visa}')
 print(f'Histórico de compras Elo Nanquim: {historico_compras_nanquim}')
 
-# mensagem_visa = f'Fechamento da próxima fatura visa: {fechamento_visa}'
-# telegram_bot(mensagem_visa)
-# mensagem_elo = f'Fechamento da próxima fatura Elo Nanquim: {fechamento_nanquim}'
-# telegram_bot(mensagem_elo)
 
-SERVICE_ACCOUNT_FILE = 'keys.json'
+SERVICE_ACCOUNT_FILE = "C:\\Users\\S027668971\\Developments\\Google-sheet\\keys.json"
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = None
 creds = service_account.Credentials.from_service_account_file(
@@ -110,6 +93,8 @@ timestamp_fechamento = time.mktime(datetime.strptime(
 validador_msg = 0
 for gasto in resultado_compra:
     numero_aba = int(gasto[0].split('/')[1]) + 1
+    mes_gasto = int(gasto[0].split('/')[1])
+    mes_fechamento_fatura = int(fechamento_planilha.split('/')[1])
     if numero_aba == 13:
             numero_aba = 1
     # if mes_gasto > numero_aba:
@@ -121,24 +106,28 @@ for gasto in resultado_compra:
                                 data_gasto, '%d/%m/%Y').timetuple())
 
     if timestamp_data_gasto < timestamp_fechamento:
-        # Mandar msg para cada gasto
-        mensagem_gasto = f'{gasto[1]} \- {gasto[-1]}'
-        if "=" in mensagem_gasto:
-            mensagem_gasto = mensagem_gasto.replace('=', '\=')
-        if "*" in mensagem_gasto:
-            mensagem_gasto = mensagem_gasto.replace('*', '\*')
-        # telegram_bot(mensagem_gasto)
 
         validador_msg = 1
-        range = f'{dicionario_meses[numero_aba]}!H2:J100'
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                            range=range).execute().get('values', [])
-        next_empty_row = len(result) + 2
-        range = f'{dicionario_meses[numero_aba]}!H{next_empty_row}'
-        sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                    range=range,
-                    valueInputOption="USER_ENTERED",
-                    body={"values": [gasto]}).execute()
+        if mes_gasto < mes_fechamento_fatura:
+            range = f'{dicionario_meses[numero_aba+1]}!H2:J100'
+            result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=range).execute().get('values', [])
+            next_empty_row = len(result) + 2
+            range = f'{dicionario_meses[numero_aba+1]}!H{next_empty_row}'
+            sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                        range=range,
+                        valueInputOption="USER_ENTERED",
+                        body={"values": [gasto]}).execute()
+        else:
+            range = f'{dicionario_meses[numero_aba]}!H2:J100'
+            result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=range).execute().get('values', [])
+            next_empty_row = len(result) + 2
+            range = f'{dicionario_meses[numero_aba]}!H{next_empty_row}'
+            sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                        range=range,
+                        valueInputOption="USER_ENTERED",
+                        body={"values": [gasto]}).execute()
     else:
         numero_aba = numero_aba + 1
         if numero_aba == 13:
@@ -169,57 +158,87 @@ for gasto in resultado_compra:
                         range=range,
                         valueInputOption="USER_ENTERED",
                         body={"values": [gasto]}).execute()
+            
+# Fechar objeto de conexão com a api do googlesheet
+sheet.close()
 
-# if not validador_msg:
-#     mensagem  = "Nenhum valor a ser adicionado a planilha\!"
-#     telegram_bot(mensagem)
-
-# print("Cartões BB atualizados!")
-# mensagem_final = "Planilha de Cartões BB atualizada\!"
-# telegram_bot(mensagem_final)
 # --------------------------------------------------------------------------------------
 
-time.sleep(5)
 # Chamando a função que pega os valores do extrato CAIXA
-# cartao_caixa = extrato_caixa()
-# data_proxima_fatura = cartao_caixa[1]
-# historico_compras = cartao_caixa[0]
+chrome = lista_definitiva[-1]
+cartao_caixa = extrato_caixa(chrome)
+data_proxima_fatura = cartao_caixa[1]
+historico_compras = cartao_caixa[0]
 
-# mensagem_caixa = f'Fechamento da próxima fatura Caixa: {data_proxima_fatura}'
-# mensagem_bot_telegram(mensagem_caixa)
+# Setar o range da aba mês - Fechamento da Fatura Caixa
+range_fechamento = f'{dicionario_meses[aba_mes]}!F5'
 
-# SERVICE_ACCOUNT_FILE = '/home/felipe/projetos_python/google_sheet/keys.json'
-# SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-# creds = None
-# creds = service_account.Credentials.from_service_account_file(
-#         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+SERVICE_ACCOUNT_FILE = "C:\\Users\\S027668971\\Developments\\Google-sheet\\keys.json"
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-# # The ID spreadsheet.
-# SAMPLE_SPREADSHEET_ID = '1aYNZ1TCIvekdr1KoBmA6D9x95TyYuEM3ycYVxtiDaJQ'
+# The ID spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1QhNbaGgEAUzUL_FhOI5xfqTe643xI7lxmpKysub8lPI'
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
 
-# service = build('sheets', 'v4', credentials=creds)
+# Inserir data de fechamento da fatura visa
+sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                    range=range_fechamento,
+                    valueInputOption="USER_ENTERED",
+                    body={"values": [[data_proxima_fatura]]}).execute()
 
-# Call the Sheets API and get the values
-# range = "api!E2:G17"
-# sheet = service.spreadsheets()
-# result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-#                             range=range).execute().get('values', [])
 
-# last_row = result[-1] if result else None
-# next_empty_row = len(result) + 2
+for gasto in historico_compras:
+    mes_gasto = int(gasto[0].split('/')[1])
+    data_gasto = gasto[0]
 
-# Upadte da Sheets
-# range = "api!E" + str(next_empty_row)
+    # Transformar data de gasto em timestamp
+    timestamp_data_gasto = time.mktime(datetime.strptime(
+                                data_gasto, '%d/%m/%Y').timetuple())
+    
+    # Pegar data de fechamento equivalente ao mês do gasto
+    range = f'{dicionario_meses[mes_gasto+1]}!F5'
+    fechamento_anterior = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                        range=range).execute().get('values', [])[0][0]
+    
+    # Transformar data de fechamento em timestamp
+    timestamp_data_fechamento = time.mktime(datetime.strptime(
+                                fechamento_anterior, '%d/%m/%Y').timetuple())
+    
+    # Condição em que determina qual mês o gasto será armazenado
+    if timestamp_data_gasto < timestamp_data_fechamento:
+        # Pegando todos os gastos armazenados e determinando a próxima linha vazia
+        range = f'{dicionario_meses[mes_gasto+1]}!B2:D100'
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=range).execute().get('values', [])
+        next_empty_row = len(result) + 2
 
-# resultado_final = []
-# for i in historico_compras:
-#     if i not in result:
-#         resultado_final.append(i)
+        # Condição que compara se gasto se foi armazenado
+        if gasto not in result:
+            # Armazenando os gastos na planilha
+            range = f'{dicionario_meses[mes_gasto+1]}!B{next_empty_row}'
+            sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                        range=range,
+                        valueInputOption="USER_ENTERED",
+                        body={"values": [gasto]}).execute()
+    else:
+        # Pegando todos os gastos armazenados e determinando a próxima linha vazia
+        range = f'{dicionario_meses[mes_gasto+2]}!B2:D100'
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=range).execute().get('values', [])
+        next_empty_row = len(result) + 2
 
-# sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-#                       range=range,
-#                       valueInputOption="USER_ENTERED",
-#                       body={"values": resultado_final}).execute()
-# print("Valor atualizado!")
-# mensagem_final = 'Planilha do Cartão Caixa atualizada!'
-# mensagem_bot_telegram(mensagem_final)
+        # Condição que compara se gasto se foi armazenado
+        if gasto not in result:
+            # Armazenando os gastos na planilha
+            range = f'{dicionario_meses[mes_gasto+2]}!B{next_empty_row}'
+            sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                        range=range,
+                        valueInputOption="USER_ENTERED",
+                        body={"values": [gasto]}).execute()
+
+# Fechar objeto de conexão com a api do googlesheet
+sheet.close()
